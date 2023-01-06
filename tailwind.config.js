25c80e186a6b27a65d7368bac061e67a84beed33
huandhl <>
2023-01-06 18:41:56 +0700

    feat: add some common variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    colors: {
+      'blue': '#1B3764',
+      'yellow': '#FFCA42',
+      'light-gray': '#F6F8FC',
+    },
     extend: {},
   },
   plugins: [],
